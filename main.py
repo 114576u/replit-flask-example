0556194e4d88ec7575ce8c9c2ecd90c6ab3e5419
Josep P <52041244+114576u@users.noreply.github.com>
2021-10-05 16:26:09 +0000

    Toggle web.authenticated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 users = web.UserStore()
 
 @app.route("/")
-# @web.authenticated
+@web.authenticated
 def index():
   hits = users.current.get("hits", 0) + 1
   users.current["hits"] = hits
